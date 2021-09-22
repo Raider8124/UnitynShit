c8ab07e85935fbffbc83e68d9fa9e831ee748c99
Raider8124 <79348713+Raider8124@users.noreply.github.com>
2021-09-21 20:51:15 -0700

    Add files via upload

---
+++ Scripts/Inventory.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+
+    public GameObject inventory;
+    public GameObject slotHolder;
+
+    private int slots;
+    private Transform[] slot;
+
+    private GameObject itemPickedUp;
+
+    public void Start()
+    {
+        // slots being detected
+        slots = slotHolder.transform.childCount;
+        slot = new Transform[slots];
+        DetectInventorySlots();
+    }
+
+    public void Update()
+    {
+        
+    }
+
+
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.GetComponent<Item>())
+        {
+            itemPickedUp = other.gameObject;
+            AddItem(itemPickedUp);
+        }
+    }
+
+
+    public void AddItem(GameObject item)
+    {
+        for (int i = 0; i < slots; i++)
+        {
+            if (slot[1].GetComponent<Slot>().empty)
+            {
+                slot[i].GetComponent<Slot>().item = itemPickedUp;
+                slot[i].GetComponent<Slot>().itemIcon = itemPickedUp.GetComponent<Item>().icon;
+            }
+        }
+    }
+
+    public void DetectInventorySlots()
+    {
+        for (int i = 0; i < slots; i++)
+        {
+            slot[i] = slotHolder.transform.GetChild(i);
+            print(slot[i].name);
+        }
+    }
+}
